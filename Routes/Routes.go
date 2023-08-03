72b737a9c42bf5f8f9f97488c0b744aae7cea431
ravaattar <rava.naufal2014@gmail.com>
2023-08-03 23:55:15 +0700

    add: initial router

---
+++ Routes/Routes.go
@@ -0,0 +1,25 @@
+package Routes
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine {
+	router := gin.Default()
+	router.NoRoute(func(c *gin.Context) {
+		c.JSON(404, gin.H{
+			"message": c.Request.URL.Path + " not found!",
+		})
+	})
+	routerGroup := router.Group("/api")
+	{
+		routerGroup.GET("/", func(c *gin.Context) {
+			println("Welcome to the Collaborative Core API (Go)!")
+			c.JSON(200, gin.H{
+				"status":  "Success",
+				"message": "Welcome to the Collaborative Core API (Go)!",
+			})
+		})
+	}
+	return router
+}
