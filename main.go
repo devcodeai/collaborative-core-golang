a782d0fd2dd730ee4e1f0cbe58c02eafcf88b190
ravaattar <rava.naufal2014@gmail.com>
2023-08-04 02:20:14 +0700

    feat: database connection on main.go

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"fmt"
 
+	"github.com/devcodeai/collaborative-core-golang/Database"
 	"github.com/devcodeai/collaborative-core-golang/Routes"
 	"github.com/devcodeai/collaborative-core-golang/Utils"
 
@@ -15,6 +16,15 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	MYSQL_USER := Utils.EnvConfig("MYSQL_USER", "root")
+	MYSQL_PASSWORD := Utils.EnvConfig("MYSQL_PASSWORD", "password")
+	MYSQL_HOST := Utils.EnvConfig("MYSQL_HOST", "localhost")
+	MYSQL_PORT := Utils.EnvConfig("MYSQL_PORT", "3306")
+	MYSQL_DBNAME := Utils.EnvConfig("MYSQL_DBNAME", "devcode")
+	DB_URL := Database.GetDatabaseURL(MYSQL_USER, MYSQL_PASSWORD, MYSQL_HOST, MYSQL_PORT, MYSQL_DBNAME)
+	
+	Database.ConnectDatabase(DB_URL)
 
 	HOST := Utils.EnvConfig("HOST", "0.0.0.0")
 	PORT := Utils.EnvConfig("PORT", "3030")
