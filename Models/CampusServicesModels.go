2477c445f6f48cf1a0b1ab9d3fbc57eba8add15c
ravaattar <rava.naufal2014@gmail.com>
2023-08-04 14:12:21 +0700

    revert: unused omitempty on models

--- Models/CampusServicesModels.go
+++ Models/CampusServicesModels.go
@@ -2,9 +2,9 @@ package Models
 
 type Campus struct {
 	ID             uint    `gorm:"primaryKey;autoIncrement;type: int not null" json:"id"`
-	UniversityName string  `gorm:"type: varchar(255) not null" json:"university_name,omitempty"`
-	Location       string  `gorm:"type: varchar(255) not null" json:"location,omitempty"`
-	Website        string  `gorm:"type: varchar(255) not null" json:"website,omitempty"`
+	UniversityName string  `gorm:"type: varchar(255) not null" json:"university_name"`
+	Location       string  `gorm:"type: varchar(255) not null" json:"location"`
+	Website        string  `gorm:"type: varchar(255) not null" json:"website"`
 	Majors         []Major `gorm:"constraint:OnDelete:CASCADE" json:"-"` // Relationship with 'Majors' table
 }
 
@@ -15,6 +15,6 @@ func (Campus) TableName() string {
 
 type Major struct {
 	ID       uint   `gorm:"primaryKey;autoIncrement;type: int not null" json:"id"`
-	Name     string `gorm:"type: varchar(255) not null" json:"name,omitempty"`
-	CampusID uint   `gorm:"type: int not null" json:"campus_id,omitempty"`
+	Name     string `gorm:"type: varchar(255) not null" json:"name"`
+	CampusID uint   `gorm:"type: int not null" json:"campus_id"`
 }
--- Models/CompanyServicesModels.go
+++ Models/CompanyServicesModels.go
@@ -2,15 +2,15 @@ package Models
 
 type Company struct {
 	ID       uint      `gorm:"primaryKey;autoIncrement;type: int not null" json:"id"`
-	Name     string    `gorm:"type: varchar(255) not null" json:"name,omitempty"`
-	Address  string    `gorm:"type: varchar(255) not null" json:"address,omitempty"`
-	Email    string    `gorm:"type: varchar(255) not null" json:"email,omitempty"`
-	Phone    string    `gorm:"type: varchar(255) not null" json:"phone,omitempty"`
+	Name     string    `gorm:"type: varchar(255) not null" json:"name"`
+	Address  string    `gorm:"type: varchar(255) not null" json:"address"`
+	Email    string    `gorm:"type: varchar(255) not null" json:"email"`
+	Phone    string    `gorm:"type: varchar(255) not null" json:"phone"`
 	Products []Product `gorm:"constraint:OnDelete:CASCADE" json:"-"` // Relationship with 'Products' table
 }
 
 type Product struct {
 	ID        uint   `gorm:"primaryKey;autoIncrement;type: int not null" json:"id"`
-	Name      string `gorm:"type: varchar(255) not null" json:"name,omitempty"`
-	CompanyID uint   `gorm:"type: int not null" json:"company_id,omitempty"`
+	Name      string `gorm:"type: varchar(255) not null" json:"name"`
+	CompanyID uint   `gorm:"type: int not null" json:"company_id"`
 }
